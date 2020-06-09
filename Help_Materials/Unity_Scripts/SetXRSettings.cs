4b2366f4bd8a40b205c5c7350d1693671b2703fd
Elena Kunau <elenakunau@Elenas-MBP.fritz.box>
2020-06-09 13:37:17 +0200

    Add Unity Help Materials

---
+++ Help_Materials/Unity_Scripts/SetXRSettings.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class SetXRSettings : MonoBehaviour
+{
+    public float TextureResolutionScale = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        XRSettings.eyeTextureResolutionScale = TextureResolutionScale;
+
+        // Activate for Oculus only:
+        // OVRManager.tiledMultiResLevel = OVRManager.TiledMultiResLevel.LMSMedium;
+    }
+
+}
